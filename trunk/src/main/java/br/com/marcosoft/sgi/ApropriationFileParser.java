511fe48e2af90b5c75ff2bdb9445cd1138785416
marcosalpereira <marcosalpereira@f667dd36-7067-0e9d-3c3d-60dcf11b08bc>
2010-10-06 19:43:08 +0000

    fixes testes funcionais

--- trunk/src/main/java/br/com/marcosoft/sgi/ApropriationFileParser.java
+++ trunk/src/main/java/br/com/marcosoft/sgi/ApropriationFileParser.java
@@ -9,9 +9,38 @@ import br.com.marcosoft.sgi.model.ApropriationFile;
 import br.com.marcosoft.sgi.model.Task;
 import br.com.marcosoft.sgi.model.TaskRecord;
 import br.com.marcosoft.sgi.util.Util;
-import static br.com.marcosoft.sgi.ColunasPlanilha.*;
 
 public class ApropriationFileParser {
+    //Posicao do tipo de registro 
+    private static final int POS_TIPO_REGISTRO = 0;
+    
+    //Tipos de registro
+    private static final String TIPO_REGISTRO_REG = "reg";
+    private static final String TIPO_REGISTRO_CFG = "cfg";
+    
+    //Registros de configuracao
+    private static final int CFG_QUANTIDADE_CAMPOS = 3;
+    private static final int POS_CFG_VALOR_PROPRIEDADE = 2;
+    private static final int POS_CFG_PROPRIEDADE = 1;    
+    
+    //Registros de registro de atividade
+    private static final int POS_REG_NUMERO_LINHA = 1;
+    private static final int POS_REG_DATA = 2;
+    @SuppressWarnings("unused")
+    private static final int POS_REG_DIA_SEMANA = 3;
+    private static final int POS_REG_AJUSTAR_INFORMACOES = 4;
+    private static final int POS_REG_UG_CLIENTE = 5;
+    private static final int POS_REG_LOTACAO_SUPERIOR = 6;
+    private static final int POS_REG_NOME_PROJETO = 7;
+    private static final int POS_REG_MACRO = 8;
+    private static final int POS_REG_TIPO_HORA = 9;
+    private static final int POS_REG_INSUMO = 10;
+    private static final int POS_REG_TIPO_INSUMO = 11;
+    private static final int POS_REG_DESCRICAO = 12;
+    private static final int POS_REG_HORA_INICIO = 13;
+    private static final int POS_REG_HORA_TERMINO = 14;
+    private static final int POS_REG_DURACAO = 15;
+    private static final int REG_QUANTIDADE_CAMPOS = 16;    
 
     private final File inputFile;
 
@@ -26,21 +55,21 @@ public class ApropriationFileParser {
         String line = null;
         while ((line = input.readLine()) != null) {
             final String[] fields = line.split("\\|");
-            if (TIPO_REGISTRO_CFG.equals(fields[COL_TIPO_REGISTRO])) {
+            if (TIPO_REGISTRO_CFG.equals(fields[POS_TIPO_REGISTRO])) {
                 if (fields.length != CFG_QUANTIDADE_CAMPOS)
                     continue;
-                ret.getConfig().put(fields[COL_CFG_PROPRIEDADE].trim(), fields[COL_CFG_VALOR_PROPRIEDADE].trim());
+                ret.getConfig().put(fields[POS_CFG_PROPRIEDADE].trim(), fields[POS_CFG_VALOR_PROPRIEDADE].trim());
 
-            } else if (TIPO_REGISTRO_REG.equals(fields[COL_TIPO_REGISTRO])) {
+            } else if (TIPO_REGISTRO_REG.equals(fields[POS_TIPO_REGISTRO])) {
                 if (fields.length != REG_QUANTIDADE_CAMPOS)
                     continue;
 
-                final int duracao = Integer.parseInt(fields[COL_REG_DURACAO]);
+                final int duracao = Integer.parseInt(fields[POS_REG_DURACAO]);
                 if (duracao == 0)
                     continue;
 
                 final TaskRecord taskRecord = new TaskRecord();
-                taskRecord.setData(Util.parseDate("dd/MM/yy", fields[COL_REG_DATA]));
+                taskRecord.setData(Util.parseDate("dd/MM/yy", fields[POS_REG_DATA]));
                 taskRecord.setDuracao(duracao);
                 taskRecord.setTask(parseTask(fields));
                 ret.getTasksRecords().add(taskRecord);
@@ -52,18 +81,18 @@ public class ApropriationFileParser {
 
     private Task parseTask(final String[] fields) {
         final Task task = new Task();
-        task.setAjustarInformacoes("Sim".equals(fields[COL_REG_AJUSTAR_INFORMACOES]));
-        task.setNumeroLinha(fields[COL_REG_NUMERO_LINHA]);
-        task.setUgCliente(fields[COL_REG_UG_CLIENTE]);
-        task.setLotacaoSuperior(!"Não".equals(fields[COL_REG_LOTACAO_SUPERIOR]));
-        task.setProjeto(fields[COL_REG_NOME_PROJETO]);
-        task.setMacro(fields[COL_REG_MACRO]);
-        task.setTipoHora(fields[COL_REG_TIPO_HORA]);
-        task.setInsumo(fields[COL_REG_INSUMO]);
-        task.setTipoInsumo(fields[COL_REG_TIPO_INSUMO]);
-        task.setDescricao(fields[COL_REG_DESCRICAO]);
-        task.setHoraInicio(fields[COL_REG_HORA_INICIO]);
-        task.setHoraTermino(fields[COL_REG_HORA_TERMINO]);
+        task.setAjustarInformacoes("Sim".equals(fields[POS_REG_AJUSTAR_INFORMACOES]));
+        task.setNumeroLinha(fields[POS_REG_NUMERO_LINHA]);
+        task.setUgCliente(fields[POS_REG_UG_CLIENTE]);
+        task.setLotacaoSuperior(!"Não".equals(fields[POS_REG_LOTACAO_SUPERIOR]));
+        task.setProjeto(fields[POS_REG_NOME_PROJETO]);
+        task.setMacro(fields[POS_REG_MACRO]);
+        task.setTipoHora(fields[POS_REG_TIPO_HORA]);
+        task.setInsumo(fields[POS_REG_INSUMO]);
+        task.setTipoInsumo(fields[POS_REG_TIPO_INSUMO]);
+        task.setDescricao(fields[POS_REG_DESCRICAO]);
+        task.setHoraInicio(fields[POS_REG_HORA_INICIO]);
+        task.setHoraTermino(fields[POS_REG_HORA_TERMINO]);
         return task;
     }
 
--- trunk/src/main/java/br/com/marcosoft/sgi/ColunasPlanilha.java
+++ trunk/src/main/java/br/com/marcosoft/sgi/ColunasPlanilha.java
@@ -1,27 +1,19 @@
 package br.com.marcosoft.sgi;
 
 public class ColunasPlanilha {
-    public static final int COL_TIPO_REGISTRO = 0;
-    public static final String TIPO_REGISTRO_REG = "reg";
-    public static final String TIPO_REGISTRO_CFG = "cfg";
-    public static final int CFG_QUANTIDADE_CAMPOS = 3;
-    public static final int COL_CFG_VALOR_PROPRIEDADE = 2;
-    public static final int COL_CFG_PROPRIEDADE = 1;
-
-    public static final int COL_REG_NUMERO_LINHA = 1;
-    public static final int COL_REG_DATA = 2;
-    public static final int COL_REG_DIA_SEMANA = 3;
-    public static final int COL_REG_AJUSTAR_INFORMACOES = 4;
-    public static final int COL_REG_UG_CLIENTE = 5;
-    public static final int COL_REG_LOTACAO_SUPERIOR = 6;
-    public static final int COL_REG_NOME_PROJETO = 7;
-    public static final int COL_REG_MACRO = 8;
-    public static final int COL_REG_TIPO_HORA = 9;
-    public static final int COL_REG_INSUMO = 10;
-    public static final int COL_REG_TIPO_INSUMO = 11;
-    public static final int COL_REG_DESCRICAO = 12;
-    public static final int COL_REG_HORA_INICIO = 13;
-    public static final int COL_REG_HORA_TERMINO = 14;
-    public static final int COL_REG_DURACAO = 15;
-    public static final int REG_QUANTIDADE_CAMPOS = 15;
+    public static final int COL_REG = 0;
+    public static final int COL_REG_DATA = 1;
+    public static final int COL_REG_DIA_SEMANA = 2;
+    public static final int COL_REG_AJUSTAR_INFORMACOES = 3;
+    public static final int COL_REG_UG_CLIENTE = 4;
+    public static final int COL_REG_LOTACAO_SUPERIOR = 5;
+    public static final int COL_REG_NOME_PROJETO = 6;
+    public static final int COL_REG_MACRO = 7;
+    public static final int COL_REG_TIPO_HORA = 8;
+    public static final int COL_REG_INSUMO =  9;
+    public static final int COL_REG_TIPO_INSUMO = 10;
+    public static final int COL_REG_DESCRICAO = 11;
+    public static final int COL_REG_HORA_INICIO = 12;
+    public static final int COL_REG_HORA_TERMINO = 13;
+    public static final int COL_REG_DURACAO = 14;
 }
