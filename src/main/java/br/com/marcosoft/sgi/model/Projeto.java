2d43a1796c7a0f7a7cf55a5c39a959bba8ed90ca
marcosalpereira <marcosalpereira@f667dd36-7067-0e9d-3c3d-60dcf11b08bc>
2012-06-15 22:30:21 +0000

    to do home work

---
+++ src/main/java/br/com/marcosoft/sgi/model/Projeto.java
@@ -0,0 +1,32 @@
+package br.com.marcosoft.sgi.model;
+
+public class Projeto {
+    private String mnemonico;
+    private String ug;
+    private boolean lotacaoSuperior;
+    private String nomeProjeto;
+    public String getMnemonico() {
+        return mnemonico;
+    }
+    public void setMnemonico(String mnemonico) {
+        this.mnemonico = mnemonico;
+    }
+    public String getUg() {
+        return ug;
+    }
+    public void setUg(String ug) {
+        this.ug = ug;
+    }
+    public boolean isLotacaoSuperior() {
+        return lotacaoSuperior;
+    }
+    public void setLotacaoSuperior(boolean lotacaoSuperior) {
+        this.lotacaoSuperior = lotacaoSuperior;
+    }
+    public String getNomeProjeto() {
+        return nomeProjeto;
+    }
+    public void setNomeProjeto(String nomeProjeto) {
+        this.nomeProjeto = nomeProjeto;
+    }
+}
